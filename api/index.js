948bbad510c76273e5bcee9f495d79d6f0d66307
Adrian Velasques <msdistribuidoraeletronicos@gmail.com>
2026-02-24 15:59:31 -0300

    fix: force express through api/index.js and use /tmp output on serverless

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 // api/index.js
 "use strict";
 
+/**
+ * Entry point da Vercel Function.
+ * Importa o Express app do arquivo raiz (app.js) e exporta direto.
+ */
 const app = require("../app.js");
+
 module.exports = app;
